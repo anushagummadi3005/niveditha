5567df9ad64574e2af7bc710eadae737f1eda6b2
anushagummadi3005 <139125442+anushagummadi3005@users.noreply.github.com>
2023-07-10 16:41:41 +0530

    Add files via upload

---
+++ Demotest.java
@@ -0,0 +1,46 @@
+package stepdefinationpackage;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Demotest {
+	@Given(": Open home Page")
+	public void open_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When(": Click on Login link")
+	public void click_on_login_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When(": Enter Username")
+	public void enter_username() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When(": Enter Password")
+	public void enter_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When(": Click on Submit")
+	public void click_on_submit() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then(": verify Logout link")
+	public void verify_logout_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+}
